2aed2ab52b0e38c417fc02574956cdda3f741f29
vipulpagar <117459705+vipulpagar@users.noreply.github.com>
2022-11-07 20:23:39 +0530

    Update info.py

---
+++ info.py
@@ -0,0 +1,17 @@
+# why pyhthon is dynamic language?
+# redability
+# easy to learn
+# no need to define datatype
+# data types :- int- 1 2 3 ; float-1.3 3.5 ;string- '',"","'"[for multiline];boolean- true or false.
+# data structuere:- list,tuple,dict,set,
+# variables are used to store the data of defined data type
+# rules of variables :- 1.variables should always start with underscore or alphabate not with number.
+# 2.no space should be there in the namea of variable.
+# 3.variables are case sensitive.
+# comments:- used as a title of code for better understanding.
+# it does not execute
+# easy readability
+# data structure:-[] for list
+#                 () tuple
+#                 {} set
+#                 {} dict  
