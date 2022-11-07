93fbd426bd935483a0f660684b9b44e7cc03a82b
vipul <pagarvipul07@gmail.com>
2022-11-07 20:49:02 +0530

    suraj

---
+++ list.py
@@ -0,0 +1,67 @@
+# list()
+a=[1,2,3,4,5,9,18,12,10]
+
+# append()
+# a.append(9)
+# print(a)
+
+
+# extend
+# a.extend([11,12,13])
+# print(a)
+
+
+# pop()
+# a.pop()
+# print(a)
+
+
+# remove()
+# a.remove(4)
+# print(a)
+
+
+# reverse()
+# a.reverse()
+# print(a)
+
+
+# sort()
+# a.sort()
+# print(a)
+
+
+# len()
+# print(len(a))
+
+
+# sum()
+# print(sum(a))
+
+
+# index()
+# print(a.index(5))
+
+
+# count()
+#print(a.count(4))
+
+
+# clear()
+# a.clear()
+# print(a)
+
+
+# copy()
+# b=a.copy()
+# print(b)
+
+
+# delete()
+# del a[5]
+# print(a)
+
+
+# insert()
+# a.insert(4,5)
+# print(a)
